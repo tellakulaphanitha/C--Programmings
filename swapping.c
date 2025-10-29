2f03fe375a12d7b3de5f6f6df347b728165f04f6
tellakulaphanitha <tellakulaphanitha@gmail.com>
2025-10-29 12:04:24 +0530

    Create swapping.c

---
+++ swapping.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+  int a,b,temp;
+printf("enter a&b values");
+scanf("%d%d",&a,&b);
+printf("before swap a=%d b=%d",a,b);
+temp=a;
+a=b;
+b=temp;
+printf("\n after swap a=%d b=%d",a,b);
+return 0;
+}
