8e7f0912513020c4f501c60b6fd2314cede4d8e3
tellakulaphanitha <tellakulaphanitha@gmail.com>
2025-10-29 12:30:52 +0530

    Create unary.c

---
+++ unary.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int main()
+{
+  int X=10,p,q,r,s;
+p=X++;
+printf("p=%d",p);
+q=--X;
+printf("q=%d",q);
+r=++X;
+printf("r=%d",r);
+S=X--;
+printf("\n S=%d",S);
+printf("\n X=%d,X);
+return 0;
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
