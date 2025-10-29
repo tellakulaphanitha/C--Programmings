b18ef9f590bc3f583a6e5a6e9eed306996680eed
tellakulaphanitha <tellakulaphanitha@gmail.com>
2025-10-29 16:49:14 +0530

    Create biggest using nested if else.c

---
+++ biggest using nested if else.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+  int a,b,c;
+printf("enter a&b&c values");
+scanf("%d%d%d",&a,&b,&c);
+if(a>b)
+{
+if(a>c)
+  printf("a is maximum");
+else
+  printf("c is maximum");
+}
+else
+{
+if(b>c)
+  printf("b is maximum");
+else
+  printf("c is maximum");
+}
+return 0;
+}
