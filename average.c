d9a3d05d73c130aeac85cc93ef649a062f0a837e
tellakulaphanitha <tellakulaphanitha@gmail.com>
2025-10-29 11:40:58 +0530

    Create average.c

---
+++ average.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+  int a,b,c,d;
+printf("enter a value");
+scanf("%d",&a);
+printf("enter b value");
+sacnf("%d",&b);
+printf("enter c value");
+scanf("%d",&c);
+d=(a+b+c)/3;
+printf("average=%d",d);
+return 0;
+}
