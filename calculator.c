8fde89260149fca74fa7e49a65ea8ce81f36b2c1
tellakulaphanitha <tellakulaphanitha@gmail.com>
2025-11-11 15:29:23 +0530

    Add files via upload

--- calculator.c
+++ calculator.c
@@ -1,27 +1,26 @@
 #include<stdio.h>
 int main()
 {
-  int a,b,c;
-char ch;
-printf("enter a&b values");
-scanf("%d%d",&a,&b);
-printf("enter your choice +add -sub *mul /div);
-scanf(" %c",&ch);
-switch(ch)
-{
-case '+':c=a+b;
-printf("add=%d",c);
-break;
-case '-':c=a-b;
-printf("sub=%d",c);
-break;
-case '*':c=a*b;
-printf("mul=%d",c);
-break;
-case '/':c=a/b;
-printf("div=%d",c);
-break;
-default: printf("invalid choice");
-}
-return 0;
+	int a,b,c;
+	char ch;
+	printf("enter a&b values");
+	scanf("%d%d",&a,&b);
+	printf("enter your choice +add -sub *mul /div");
+	scanf("%c",&ch);
+	switch(ch)
+	{
+		case '+':c=a+b;
+		         printf("addition=%d",c);
+		         break;
+	    case '-':c=a-b;
+	             printf("subtraction=%d",c);
+	             break;
+	    case '*':c=a*b;
+	             printf("multiplication=%d",c);
+	             break;
+	    case '/':c=a/b;
+	             printf("division=%d",c);
+	    default:printf("invalid choice");
+	}
+	return 0;
 }
