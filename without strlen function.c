2413cca32f31d001df283b67755b6b0c8029ce66
tellakulaphanitha <tellakulaphanitha@gmail.com>
2025-11-16 06:55:59 +0530

    Add files via upload

---
+++ without strlen function.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char s[20];
+	int i,l;
+	printf("enter string name");
+	gets(s);
+	for(i=0;s[i]!='\0';i++);
+	l=i;
+	printf("length of the string =%d",l);
+	return 0;
+}
